bf075f53bada04cbad9cb6c4b680825afbb2818b
Khan B <itisameerkhan@gmail.com>
2025-05-23 13:20:29 +0530

    first commit

---
+++ src/pages/Auth/Auth.jsx
@@ -0,0 +1,159 @@
+import { useState } from "react";
+import "./Auth.scss";
+import axios from "axios";
+import { ToastContainer, toast } from "react-toastify";
+import { useNavigate } from "react-router-dom";
+
+const Auth = () => {
+  const navigate = useNavigate();
+  const [auth, setAuth] = useState("signup");
+  const [user, setUser] = useState({
+    name: "",
+    email: "",
+    password: "",
+    age: "",
+    phoneNumber: "",
+  });
+
+  const notify = (message) => toast(message);
+
+  const handleChange = (e) => {
+    setUser({
+      ...user,
+      [e.target.name]: e.target.value,
+    });
+  };
+
+  const handleSubmit = async () => {
+    if (auth === "signup") {
+      if (
+        !user.name ||
+        !user.password ||
+        !user.age ||
+        !user.phoneNumber ||
+        !user.email
+      ) {
+        notify("All fields required");
+        return;
+      }
+    }
+    if (auth === "login") {
+      if (!user.email || !user.password) {
+        notify("All fields required");
+        return;
+      }
+    }
+    try {
+      if (auth === "signup") {
+        const response = await axios.post(
+          import.meta.env.VITE_HOST_URL + "/user/new",
+          user,
+          { withCredentials: true }
+        );
+
+        if (!response.data.success) {
+          notify(response.data.message);
+          return;
+        }
+
+        navigate("/home");
+      } else if (auth === "login") {
+        const response = await axios.post(
+          import.meta.env.VITE_HOST_URL + "/user/login",
+          {
+            email: user.email,
+            password: user.password,
+          },
+          { withCredentials: true }
+        );
+
+        if (!response.data.success) {
+          notify(response.data.message);
+          return;
+        }
+
+        navigate("/home");
+      }
+    } catch (e) {
+      console.log(e);
+    }
+  };
+
+  return (
+    <div className="auth-main">
+      <div className="auth">
+        <h2>{auth === "signup" ? "Sign up" : "Login"}</h2>
+        <div className="auth-content">
+          {auth === "signup" && (
+            <div className="a-c-c">
+              <label>name</label>
+              <input
+                type="text"
+                name="name"
+                value={user.name}
+                onChange={handleChange}
+              />
+            </div>
+          )}
+          <div className="a-c-c">
+            <label>email</label>
+            <input
+              type="email"
+              name="email"
+              value={user.email}
+              onChange={handleChange}
+            />
+          </div>
+          {auth === "signup" && (
+            <div className="a-c-c">
+              <label>phoneNumber</label>
+              <input
+                type="number"
+                name="phoneNumber"
+                value={user.phoneNumber}
+                onChange={handleChange}
+              />
+            </div>
+          )}
+          {auth === "signup" && (
+            <div className="a-c-c">
+              <label>age</label>
+              <input
+                type="number"
+                name="age"
+                value={user.age}
+                onChange={handleChange}
+              />
+            </div>
+          )}
+          <div className="a-c-c">
+            <label>password</label>
+            <input
+              type="password"
+              name="password"
+              value={user.password}
+              onChange={handleChange}
+            />
+          </div>
+        </div>
+        <p
+          className="a-user"
+          onClick={() => {
+            const authTemp = auth === "signup" ? "login" : "signup";
+            setAuth(authTemp);
+          }}
+        >
+          {auth === "signup"
+            ? "Already having an account? login"
+            : "new user? Sign up"}
+        </p>
+        <button className="auth-btn" onClick={handleSubmit}>
+          {auth === "signup" ? "Sign up" : "Login"}
+        </button>
+      </div>
+      <ToastContainer />
+    </div>
+  );
+};
+
+export default Auth;
